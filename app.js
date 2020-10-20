582798ad514230ca5e7d30ab763cb4df06d3d34a
wym <731162160@qq.com>
2020-10-20 22:47:44 +0800

    change wrong variable spell

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 // var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let intellScrennRouter = require('./routes/intell-screen');
+let intellScreenRouter = require('./routes/intell-screen');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/scm', intellScrennRouter);
+app.use('/scm', intellScreenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
